44e69970e3cd2250323ff3406f79c04e5eeca364
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-12-04 19:01:39 +0100

    Don't strip symbols from 200-game

--- 200-game/make.bat
+++ 200-game/make.bat
@@ -3,7 +3,7 @@ rem Download and install raylib_installer_v300.mingw.exe from https://github.com
 set RAYLIB_PATH=C:\raylib\raylib
 set COMPILER_PATH=C:\raylib\mingw\bin
 set CC=%COMPILER_PATH%\gcc
-set CFLAGS=%RAYLIB_PATH%\src\raylib.rc.data -s -static -O0 -std=c99 -mwindows -Wall -Iexternal -DPLATFORM_DESKTOP
+set CFLAGS=%RAYLIB_PATH%\src\raylib.rc.data -static -O0 -std=c99 -mwindows -Wall -Iexternal -DPLATFORM_DESKTOP
 set LDFLAGS=-lraylib -lopengl32 -lgdi32 -lwinmm
 
 echo %CC% game.c -o game.exe %CFLAGS% %LDFLAGS%
